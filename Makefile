bc6dacc18678d7bac9e513b294cf564b516ff191
Will Dicharry <wdicharry@gmail.com>
2015-08-30 14:53:43 -0600

    Status LED blinks.

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
 
 CC=arm-none-eabi-gcc
 CFLAGS=-O2 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles
+OBJCOPY=arm-none-eabi-objcopy
+
+all: kernel.img
 
 kernel.elf: main.c
 	$(CC) $(CFLAGS) main.c -o kernel.elf
+
+kernel.img: kernel.elf
+	$(OBJCOPY) kernel.elf -O binary kernel.img
+
+clean:
+	rm kernel.elf
+	rm kernel.img
--- main.c
+++ main.c
@@ -1,8 +1,39 @@
+#include "definitions.h"
+
+/* GPIO Register Set  */
+volatile unsigned int* gpio;
+
+/* Loop variable */
+volatile unsigned int tim;
+
+int main(void) __attribute__((naked));
 int main(void) {
 
+    /* Assign the ARM physical address of the GPIO perpheral */
+    gpio = (unsigned int*)GPIO_BASE;
+
+    /* Enable GPIO 16 as an output by writing a 1 to the function select
+     * 1 GPIO register
+     */
+    gpio[LED_GPFSEL] |= (1 << LED_GPFBIT);
+
     while (1) {
+
+        /* Wait */
+        for (tim = 0; tim < 500000; tim++)
+            ;
+
+        /* Set the LED GPIO pin low */
+        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
+
+        /* Wait */
+        for (tim = 0; tim < 500000; tim++)
+            ;
+
+        /* Set the LED GPIO pin high */
+        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+
     }
-    return 0;
 }
 
 void exit(int code) {
